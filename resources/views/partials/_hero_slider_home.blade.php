eaaff9c978fb8c08cfa30ab4838638d3fb481953
Amit <amit@apdigital.asia>
2025-01-03 16:58:30 +0700

    tutorials link fix

--- resources/views/partials/_hero_slider_home.blade.php
+++ resources/views/partials/_hero_slider_home.blade.php
@@ -31,7 +31,7 @@
 					<p>Gain access to a wealth of resources, tutorials, and expert guidance at no cost.</p>
 					<p>We believe in making high-quality education and support to be accessible to everyone</p>
 					<a class="cd-btn devapp">Development Services</a>
-					<a data-link="/home/public/tutorials" class="cd-btn learn selected changePage">Let's launch into learning!</a>
+					<a data-link="/tutorials" class="cd-btn learn selected changePage">Let's launch into learning!</a>
 					<a class="cd-btn consult">Consultancy Services</a>
 
 				</div>
