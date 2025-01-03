06a44a58d51768b96b5019d1f98ba40281eebfe0
Amit <amit@apdigital.asia>
2025-01-03 16:37:33 +0700

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
 
 
 Route::get('/tutorials', function () {
     return view('tutorials');
-});
+})->name('tutorials');
+
+Route::get('/privacy-policy', function () {
+    return view('privacy-policy');
+})->name('privacy-policy');
+
 
 Route::get('/tutorials/videos', function () {
     return view('tutorials.videos');
-});
+})->name('tutorials-videos');
+
+Route::get('/logo', function () {
+    return view('partials._logo');
+})->name('logo');
 
 
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+})->name('portfolio');
+
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
