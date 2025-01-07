874318b3fe2bcdff08d5003372540b65cb7e7ef9
Amit <amit@apdigital.asia>
2025-01-07 18:13:49 +0700

    update video to Video 7 seed fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-
+use App\Models\video;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -21,10 +21,19 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        
 
-        $this->call([
-            VideoSeeder::class,
+
+        // $this->call([
+        //     VideoSeeder::class,
+        // ]);
+
+        Video::factory()->create([
+            'youTubeId' => "abcdef",
+            'title' => "This is a title",
+            'description' => 'This is a description field',
+            'tags' => "tag1, tag2",
+            'url' => 'http://wwww.google.com/',
+            'thumbnailURL' => 'http://wwww.google.com/',
         ]);
     }
 }
