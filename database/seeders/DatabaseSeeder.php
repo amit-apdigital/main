960057b72ef82cca00e7e0ab46d443ecc1c00429
Amit <amit@apdigital.asia>
2025-01-07 16:48:36 +0700

    seeder fix 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Video;
+use App\Models\video;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
