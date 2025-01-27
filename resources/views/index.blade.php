f891de6911aac76c5f52d923d9252141ec8d3345
Amit Patel <amit@apdigital.asia>
2025-01-27 18:12:18 +0700

    homepage update 9

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,13 @@
     <article class="consultancy grid-container full ">
         <div class="grid-container grid-padding-x grid-padding-y">
             <div class="cell small-6">
+                <a class="cd-btn consult">Consultancy Services</a>
+
+            </div>
+            <div class="cell small-6">
                 <h2>LETS STEP UP YOUR BUSINESS IT SOLUTIONS</h2>
                 <p>Design, develop, and implement end to end custom IT business solutions.</p>
                 <p>We offer affordable, straightforward solutions for your business in its early stages.</p>
-                <a class="cd-btn consult">Consultancy Services</a>
             </div>
         </div>
     </article>
@@ -19,7 +22,10 @@
                 <h2>TIGHT BUDGET?<br />NO WORRIES!</h2>
                 <p>Why pay expensive ongoing monthly fees for software features you dont need?</p>
                 <p>Let us work together and take your business to new heights.</p>
+            </div>
+            <div class="cell small-6">
                 <a class="cd-btn devapp">Development Services</a>
+
             </div>
         </div>
     </article>
