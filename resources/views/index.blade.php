14396bb028aa310439f96d1b8db738e3498fe7b0
Amit Patel <amit@apdigital.asia>
2025-01-27 18:17:43 +0700

    homepage update 10

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,28 +4,32 @@
     @include('partials._hero_slider_home')
 
     <article class="consultancy grid-container full ">
-        <div class="grid-container grid-padding-x grid-padding-y">
-            <div class="cell small-6">
-                <a class="cd-btn consult">Consultancy Services</a>
+        <div class="grid-container">
 
-            </div>
-            <div class="cell small-6">
-                <h2>LETS STEP UP YOUR BUSINESS IT SOLUTIONS</h2>
-                <p>Design, develop, and implement end to end custom IT business solutions.</p>
-                <p>We offer affordable, straightforward solutions for your business in its early stages.</p>
+            <div class="grid-x grid-padding-x grid-padding-y">
+                <div class="cell small-6">
+                    <a class="cd-btn consult">Consultancy Services</a>
+                </div>
+                <div class="cell small-6">
+                    <h2>LETS STEP UP YOUR BUSINESS IT SOLUTIONS</h2>
+                    <p>Design, develop, and implement end to end custom IT business solutions.</p>
+                    <p>We offer affordable, straightforward solutions for your business in its early stages.</p>
+                </div>
             </div>
         </div>
     </article>
     <article class="development grid-container full">
-        <div class="grid-container grid-padding-x grid-padding-y">
-            <div class="cell small-6">
-                <h2>TIGHT BUDGET?<br />NO WORRIES!</h2>
-                <p>Why pay expensive ongoing monthly fees for software features you dont need?</p>
-                <p>Let us work together and take your business to new heights.</p>
-            </div>
-            <div class="cell small-6">
-                <a class="cd-btn devapp">Development Services</a>
+        <div class="grid-container">
+            <div class="grid-x grid-padding-x grid-padding-y">
+                <div class="cell small-6">
+                    <h2>TIGHT BUDGET?<br />NO WORRIES!</h2>
+                    <p>Why pay expensive ongoing monthly fees for software features you dont need?</p>
+                    <p>Let us work together and take your business to new heights.</p>
+                </div>
+                <div class="cell small-6">
+                    <a class="cd-btn devapp">Development Services</a>
 
+                </div>
             </div>
         </div>
     </article>
