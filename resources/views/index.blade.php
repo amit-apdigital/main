157f44c8839056288b93e7992d3f79305e48e17f
Amit Patel <amit@apdigital.asia>
2025-01-27 14:20:31 +0700

    undo video updates 5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,10 @@
 @include('partials._header')
 <section>
 
-    @include('partials._hero_slider_home')
+@include('partials._hero_slider_home')
 
     <article class="grid-container fluid ">
         <div class="grid-x">
-            <div class="cell ">
-                <h2>LETS STEP UP YOUR BUSINESS IT SOLUTIONS</h2>
-                <p>Design, develop, and implement end to end custom IT business solutions.</p>
-                <p>We offer affordable, straightforward solutions for your business in its early stages.</p>
-                <a class="cd-btn devapp selected">Let's get started</a>
-                <a class="cd-btn learn">100% Free learning</a>
-                <a class="cd-btn consult">Consultancy Services</a>
-            </div>
-            <div class="cell">
-                <h2>TIGHT BUDGET?<br />NO WORRIES!</h2>
-                <p>Why pay expensive ongoing monthly fees for software features you dont need?</p>
-                <p>Let us work together and take your business to new heights.</p>
-                <a class="cd-btn devapp">Development Services</a>
-                <a class="cd-btn learn">100% Free learning</a>
-                <a class="cd-btn consult selected">Lets start customizing</a>
-
-            </div>
-
         </div>
     </article>
 </section>
