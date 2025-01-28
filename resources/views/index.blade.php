66425ee1105cac844d3fa7c7298b2b16c039c888
Amit Patel <amit@apdigital.asia>
2025-01-28 14:35:42 +0700

    homepage  update 8

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,10 +7,10 @@
         <div class="grid-container">
 
             <div class="grid-x grid-padding-x grid-padding-y">
-                <div class="cell small-6">
+                <div class="cell small-6-auto">
                     <a class="cd-btn consult"><img src="/images/consultancy.jpg" title="Consultancy" /></a>
                 </div>
-                <div class="cell small-6">
+                <div class="cell small-6-auto">
                     <h2>LETS STEP UP YOUR BUSINESS IT SOLUTIONS</h2>
                     <p>We can design, develop, and implement end to end custom IT business solutions to improve business performance and reduce overheads, increasing that all important profit margin.</p>
                     <p>We offer affordable, straightforward solutions for your business whether its its early stages or already well established. </p>
@@ -21,12 +21,12 @@
     <article class="development grid-container full">
         <div class="grid-container">
             <div class="grid-x grid-padding-x grid-padding-y">
-                <div class="cell small-6">
+                <div class="cell small-6-auto">
                     <h2>TIGHT BUDGET?<br />NO WORRIES!</h2>
                     <p>Why pay expensive ongoing monthly fees for software features you dont need?</p>
                     <p>We can  work together with you  and take your business software needs to new heights.</p>
                 </div>
-                <div class="cell small-6">
+                <div class="cell small-6-auto">
                     <a class="cd-btn devapp"><img src="/images/development.jpg" title="Development" /></a>
 
                 </div>
