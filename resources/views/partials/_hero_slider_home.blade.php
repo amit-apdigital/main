6824f51a60e1864564eb4f5b206c66a14197727b
Amit Patel <amit@apdigital.asia>
2025-01-28 15:02:04 +0700

    homepage  update 12

--- resources/views/partials/_hero_slider_home.blade.php
+++ resources/views/partials/_hero_slider_home.blade.php
@@ -11,7 +11,7 @@
 					<h2>LEARN NEW SKILLS</h2>
 					<p>Gain access to a wealth of resources, tutorials, and expert guidance at no cost.</p>
 					<p>We believe in making high-quality education and support to be accessible to everyone</p>
-					<a data-link="/tutorials" class="cd-btn learn">Let's launch into learning!</a>
+					<a data-link="/tutorials" class="cd-btn learn changePage">Let's launch into learning!</a>
 					<a href="#consultancy" class="cd-btn consult">Consultancy Services</a>
 					<a href="#development" class="cd-btn devapp">Development Services</a>
 
