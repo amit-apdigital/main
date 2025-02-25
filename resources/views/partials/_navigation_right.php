325e13068ed969ed9f6e7a2e042fe9dab7358931
Amit Patel <amit@apdigital.asia>
2025-02-25 20:36:07 +0700

    installing laravel fortify  register fix2

--- resources/views/partials/_navigation_right.php
+++ resources/views/partials/_navigation_right.php
@@ -2,23 +2,3 @@
     <a class="changePage" data-link="/tutorials">Tutorials</a>
 </li>
 
-@if (Route::has('login'))
-<li>
-    @auth
-    @else
-<li class="login">
-    <a class="changePage" data-link="/login">Log in</a>
-    <!-- <a class="changePage" data-link="/login" href="{{ route('login') }}" class="">Log in</a> -->
-
-</li>
-
-@if (Route::has('register'))
-<li>
-     <a class="changePage" data-link="/register">Register</a>
-
-    <!-- <a href="{{ route('register') }}" class="">Register</a> -->
-</li>
-@endif
-@endauth
-
-@endif
