25644d69006a31c2efb456b2c9d5771ad529ab64
Amit Patel <amit@apdigital.asia>
2025-02-25 13:16:33 +0700

    installing laravel fortify login register links fix 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 
 Route::get('/', function () {
     return view('index');
