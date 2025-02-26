6d41a6adef4ef95cbe02ee3075803cd0226decd0
Amit Patel <amit@apdigital.asia>
2025-02-26 12:08:26 +0700

    installing laravel fortify  register login 9

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,7 +25,7 @@
                             </div>
                         </div>
 
-                        <div class="card-section">
+                        <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
@@ -39,7 +39,7 @@
                             </div>
                         </div>
 
-                        <div class="card-section">
+                        <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -51,7 +51,7 @@
                             </div>
                         </div>
 
-                        <div class="card-section">
+                        <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="cd-btn btn-primary">
                                     {{ __('Login') }}
