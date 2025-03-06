83033bbe54938115f21a23ab461b21a35db0c1ba
Amit Patel <amit@apdigital.asia>
2025-03-06 10:39:54 +0700

    homepage update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,11 +7,11 @@
         <div class="grid-container">
 
             <div class="grid-x grid-padding-x grid-padding-y">
-                <div class="cell small-6-auto">
+                <div class="cell small-6">
                     <a data-link="/portfolio" id="consultancy" class="cd-btn consult changePage"><img src="/images/consultancy.jpg" title="Consultancy" /></a>
                     <h2 style="position: relative;top: -120px;background: rgba(255, 255, 255, 0.25);display: inline;padding: 1rem;">LETS STEP UP YOUR BUSINESS IT SOLUTIONS</h2>
                 </div>
-                <div class="cell small-6-auto">
+                <div class="cell small-6">
                     <p>We can design, develop, and implement end to end custom IT business solutions to improve business performance and reduce overheads, increasing that all important profit margin.</p>
                     <p>We offer affordable, straightforward solutions for your business whether its its early stages or already well established. </p>
                 </div>
@@ -21,13 +21,13 @@
     <article class="development grid-container full">
         <div class="grid-container">
             <div class="grid-x grid-padding-x grid-padding-y">
-                <div class="cell small-6-auto">
+                <div class="cell small-6">
                     <a id="development"></a>
                     <h2>TIGHT BUDGET? NO WORRIES!</h2>
                     <p>Why pay expensive ongoing monthly fees for software features you dont need?</p>
                     <p>We can work together with you and take your business software needs to new heights.</p>
                 </div>
-                <div class="cell small-6-auto">
+                <div class="cell small-6">
                     <a data-link="/portfolio" class="cd-btn devapp changePage"><img src="/images/development.jpg" title="Development" /></a>
 
                 </div>
