2ab10afef0430ea7859555b663788ff6b3a10f27
Amit Patel <amit@apdigital.asia>
2025-03-06 12:47:36 +0700

    homepage update 5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,16 @@
 @include('partials._header')
 <section>
-
-
     @include('partials._hero_slider_home')
     <article class="headlineText grid-container full ">
         <div class="grid-container ">
-
             <div class="grid-x grid-padding-x grid-padding-y">
-                <div class="cell small-6">
+                <div class="cell small-6-auto">
                     <p>Custom IT business solutions</p>
                 </div>
             </div>
     </article>
     <article class="consultancy grid-container full ">
         <div class="grid-container">
-
             <div class="grid-x grid-padding-x grid-padding-y">
                 <div class="cell small-6">
                     <a data-link="/portfolio" id="consultancy" class="consult changePage"><img src="/images/consultancy.png" title="Consultancy" /></a>
@@ -38,16 +34,9 @@
                 </div>
                 <div class="cell small-6">
                     <a data-link="/portfolio" class="devapp changePage"><img src="/images/development.jpg" title="Development" /></a>
-
                 </div>
             </div>
         </div>
     </article>
 </section>
-
-
-
-
-
-
 @include('partials._footer')
